519c9e51ee2b5ae766397ad7313cad698bc2e61d
Tushar Singh <tusharsingh432@gmail.com>
2021-06-17 20:54:13 +0530

    Error Handle: Uncaught ones

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const dotenv = require(`dotenv`);
 const mongoose = require(`mongoose`);
 dotenv.config({ path: `${__dirname}/configure.env` });
 //console.log(process.env);
-const server = require("./app");
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    process.exit(1);
+})
+
+const app = require("./app");
 
 const db = process.env.DATABASE;
 
@@ -13,12 +18,19 @@ mongoose.connect(db, {
     useUnifiedTopology: true
 }).then(() => {
     console.log(`Connected to Database...`);
-}).catch((err) => {
-    console.log(err);
 })
 
 const port = process.env.PORT;
-server.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`The app has started in ${process.env.NODE_ENV} mode`);
     console.log(`Listening on ${port}...`);
 });
+
+process.on('unhandledRejection', (err, promise) => {
+
+    console.log(err.name, err.message);
+    //console.log(`Called unhandledRejection`);
+    server.close(() => {
+        process.exit(1);
+    })
+})
